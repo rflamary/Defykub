b1d8066bf8881aea808504e8718b412fcdd7db93
Rémi Flamary <remi.flamary@gmail.com>
2012-01-30 21:39:36 +0100

    filename change

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(ROOT, 'README')).read()
 
 setup(name='Defykub',
       version='0.1',
       description='A puzzle game similar to Ricochet Robot',
-      author='Remi Flamary',
+      long_description=README,
+      author=u'Rémi Flamary',
       author_email='remi.flamary@gmail.com',
-      url='',
+      url='https://github.com/flam157/Defykub/',
+      py_modules=['defykub','defykub-pygame'],
+      scripts=['scripts/defykub'],
+      license = 'GPL',
       packages=['pgu'],
+      requires=["pygame (>=0.1.9)"],
      )
